a913a57407e6ad4fc0bb6e44596cdd1d614ed400
drmorzek <drmorzek@mail.ru>
2020-05-03 02:52:58 +0300

    replace

--- api/src/routes/mainRoute.js
+++ api/src/routes/mainRoute.js
@@ -1,32 +1,20 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 
+const mainMiddleware = require("../utils/mainMiddleware")
+
 const app = express();
+
+
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
     extended: true
 }));
-app.use((req, res, next) => {
-    console.log({
-        method: req.method,
-        ip: req.ip,
-        path: req.path,
-        params: req.params.param,
-        query: req.query,
-        body: req.body
-    });
-    console.log('Time:', new Date());
-    next();
-});
-app.use((error, req, res, next) => {
-    console.error(error);
-    res.status(500).send({
-        message: "Что-то пошло не так",
-        type: String(error.type),
-        error: String(error.message),
-    });
-});
+
+app.use(mainMiddleware.logResponse);
+
+app.use(mainMiddleware.sendError);
 
 module.exports = {
     app: app,
---
+++ api/src/utils/mainMiddleware.js
@@ -0,0 +1,44 @@
+exports.logResponse = (req, res, next) => {
+    const oldWrite = res.write;
+    const oldEnd = res.end;
+
+    const chunks = [];
+
+    res.write = (...restArgs) => {
+        chunks.push(Buffer.from(restArgs[0]));
+        oldWrite.apply(res, restArgs);
+    };
+
+    res.end = (...restArgs) => {
+        if (restArgs[0]) {
+            chunks.push(Buffer.from(restArgs[0]));
+        }
+        const body = Buffer.concat(chunks).toString('utf8');
+
+        console.log({
+            time: new Date().toUTCString(),
+            fromIP: req.headers['x-forwarded-for'] ||
+                req.connection.remoteAddress,
+            method: req.method,
+            originalUri: req.originalUrl,
+            uri: req.url,
+            requestData: req.body,
+            responseData: body,
+            referer: req.headers.referer || '',
+            ua: req.headers['user-agent']
+        });
+
+        oldEnd.apply(res, restArgs);
+    };
+    next();
+}
+
+exports.sendError = (error, req, res, next) => {
+    console.error(error);
+    res.status(500).send({
+        message: "Что-то пошло не так",
+        type: String(error.type),
+        error: String(error.message),
+    });
+    next();
+}
