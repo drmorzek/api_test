2bcaf3a17e57957d652969ba8b268078349969a0
drmorzek <drmorzek@mail.ru>
2020-05-02 22:17:55 +0300

    first commit

---
+++ api/src/routes/authRoute.js
@@ -0,0 +1,21 @@
+const mainroute = require("./mainRoute")
+
+const authsRouter = mainroute.express.Router();
+
+authsRouter.get('/', (req, res) => {
+    res.send('startpage');
+});
+
+authsRouter.get("/register", (req, res) => {
+    res.send('register');
+});
+
+authsRouter.get("/login", (req, res) => {
+    res.send('login');
+});
+
+authsRouter.get("/logout", (req, res) => {
+    res.send('logout');
+});
+
+module.exports = authsRouter;
--- api/src/server.js
+++ api/src/server.js
@@ -1,15 +1,18 @@
 const app = require("./routes/mainRoute").app;
+
+const authRouter = require("./routes/authRoute");
 const newsRouter = require("./routes/newsRoute");
 const usersRouter = require("./routes/usersRoute");
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello API');
-});
 
+
+app.use("/", authRouter);
 app.use("/news", newsRouter);
 app.use("/users", usersRouter);
+
+
 
 app.listen(port, () => {
     console.log(`Сервер запустился на порту ${port}...`);
