afccddfd0f73526de12ef5d9077eb3f6cdac5259
drmorzek <drmorzek@mail.ru>
2020-05-04 14:55:23 +0300

    replace

--- api/src/utils/sessionMiddleware.js
+++ api/src/utils/sessionMiddleware.js
@@ -3,12 +3,14 @@ const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const jwt = require('../models/jwt')();
 const session_secret = jwt.gen_super_key().get_secret();
+const FileStore_secret = jwt.gen_super_key().get_secret();
 
 const session_config = {
     
     secret: session_secret,
     store: new FileStore({
-        path: "./src/utils/session/"
+        path: "./src/utils/session/",
+        secret: FileStore_secret
     }),
     cookie: {
         path: './cookies/',
