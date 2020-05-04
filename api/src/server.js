f7360f9d129758b9b70c26ed91c00d7d25f758c3
drmorzek <drmorzek@mail.ru>
2020-05-04 09:40:39 +0300

    replace

--- api/src/server.js
+++ api/src/server.js
@@ -7,22 +7,23 @@ const authRouter = require("./routes/authRoute");
 
 const JWT = require("./models/jwt");
 
-
+var payload = {
+    "awfgsagsg": 2423523,
+    "fffffffffff": "afesagfagag"
+};
 
 // const JWT2 = new JWT();
 console.log("==============Сгенерирован ключ====================")
 const JWT1 = JWT();
 console.log(JWT1.new().get());
-console.log(JWT1.get_token({"awfgsagsg":2423523}));
+console.log(JWT1.get_token(payload));
 console.log(JWT1.verify());
 
 console.log("===============Свой ключ===================")
 const JWT2 = JWT();
 JWT2.set("afaenbguygehfuy");
 console.log(JWT2.get());
-console.log(JWT2.get_token({
-    "awfgsagsg": 2423523
-}));
+console.log(JWT2.get_token(payload));
 console.log(JWT2.verify());
 // console.log(JWT2.new().get());
 
